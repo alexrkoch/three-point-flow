3eef6550f391434a58fb21a60702d37edd369c81
Alex <alexrkoch@gmail.com>
2022-02-16 15:43:43 -0500

    add head rank functionality

--- flow_solver_test.py
+++ flow_solver_test.py
@@ -4,9 +4,12 @@ import pandas as pd
 from geopy import distance
 import flow_solver as fs
 
-# def test_add():
-#   assert add(1,2) == 3
 
 def test_data_has_numbers():
   df = fs.load_data()
   assert type(df.iloc[0]) == int or float
+
+def test_define_head_rank():
+  df = fs.load_data()
+  df = fs.define_head_rank(df)
+  assert df.loc[1.0].iloc[2] >= df.loc[2.0].iloc[2] >= df.loc[3.0].iloc[2]
