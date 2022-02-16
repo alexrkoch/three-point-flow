e313948e76a4381a0041a25d422ffb171e01cf45
Alex <alexrkoch@gmail.com>
2022-02-16 11:10:33 -0500

    add program outline steps

--- flow-solver.py
+++ flow-solver.py
@@ -1,1 +1,16 @@
-print("Hello World!!!")
+import numpy as np
+import pandas as pd
+
+# Check for import csv file
+  # If Exists: Import data from csv
+  # If Doesn't Exist: Import data from sample csv
+
+# Verify numbers are acceptable (all are numbers, coordinates are positive)
+
+# Identify which wells have the highest lowest and middle head 
+
+# Calculate distance between highest and lowest
+
+# Calculate point on that line of equipotential with middle head
+
+# Calculate azimuthal direction of flow normal to the equipotential line
