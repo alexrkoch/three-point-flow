d6338bd3ac329bcd7c18e9d423d70ae626888ae8
Alex <alexrkoch@gmail.com>
2022-02-16 16:14:00 -0500

    add highest to lowest head distance calc

--- flow_solver.py
+++ flow_solver.py
@@ -11,29 +11,27 @@ def load_data():
     print("No input data found, using sample data.")
   return df
 
-df = load_data()
-
 def define_head_rank(df):
   # highest head 'head_rank'= 1.0, lowest = 3.0
   df['head_rank'] = df.iloc[:, 2].rank(ascending = 0)
   df = df.set_index('head_rank')
   return df
 
+def length_highest_to_lowest(df):
+  highest = (df.loc[1.0].iloc[0], df.loc[1.0].iloc[1])
+  lowest = (df.loc[3.0].iloc[0], df.loc[3.0].iloc[1])
+  length = distance.distance(highest, lowest).feet
+  return length
+
+
+
+df = load_data()
 df = define_head_rank(df)
-print(df.head())
+print(length_highest_to_lowest(df))
 
-
-
-# Verify numbers are acceptable (all are numbers, coordinates are positive), leaving this to the end
-
-# Identify which wells have the highest lowest and middle head 
-
-# Calculate distance between highest and lowest using geopy
-  # geopy.distance.distance example:
-    # newport_ri = (41.49008, -71.312796)
-    # cleveland_oh = (41.499498, -81.695391)
-    # print(distance.distance(newport_ri, cleveland_oh).feet)
 
 # Calculate point on that line of equipotential with middle head
 
 # Calculate azimuthal direction of flow normal to the equipotential line
+
+# Verify numbers are acceptable (all are numbers, coordinates are positive), leaving this to the end
