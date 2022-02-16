e6147b087e8116f826bdfae789d6f2d4e14b44c7
Alex <alexrkoch@gmail.com>
2022-02-16 11:39:52 -0500

    add geopy distance calculator example

--- flow-solver.py
+++ flow-solver.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from geopy import distance
+
+
 
 # Check for import csv file
   # If Exists: Import data from csv
@@ -9,7 +12,12 @@ import pandas as pd
 
 # Identify which wells have the highest lowest and middle head 
 
-# Calculate distance between highest and lowest
+# Calculate distance between highest and lowest using geopy
+  # geopy.distance.distance example:
+    # newport_ri = (41.49008, -71.312796)
+    # cleveland_oh = (41.499498, -81.695391)
+    # print(distance.distance(newport_ri, cleveland_oh).feet)
+
 
 # Calculate point on that line of equipotential with middle head
 
--- flow-solver_test.py
+++ flow-solver_test.py
@@ -1,2 +1,4 @@
 import pytest
-
+import numpy as np
+import pandas as pd
+from geopy import distance
