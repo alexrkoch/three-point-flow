1f59b92ad3412222bb8a75931071f5a8734f7f0d
Alex <alexrkoch@gmail.com>
2022-02-17 10:41:33 -0500

    add bearing test

--- flow_solver_test.py
+++ flow_solver_test.py
@@ -14,8 +14,14 @@ def test_define_head_rank():
   df = fs.define_head_rank(df)
   assert df.loc[1.0].iloc[2] >= df.loc[2.0].iloc[2] >= df.loc[3.0].iloc[2]
 
-def test_length_highest_to_lowest():
+def test_length_low_to_high():
   df = fs.load_data()
   df = fs.define_head_rank(df)
-  length = fs.length_highest_to_lowest(df)
+  length = fs.length_low_to_high(df)
   assert length == 264.9634085161976
+
+def test_bearing_low_to_high():
+  df = fs.load_data()
+  df = fs.define_head_rank(df)
+  bearing = fs.bearing_low_to_high(df)
+  assert bearing == 286.1800749096567
