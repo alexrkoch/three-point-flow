384243181027a54f8f246661d6af5aa2da99a54f
Alex <alexrkoch@gmail.com>
2022-02-20 15:23:32 -0500

    refactor test_define_head_rank

--- flow_solver_test.py
+++ flow_solver_test.py
@@ -4,70 +4,62 @@ from geopy import distance, point
 import flow_solver as fs
 
 
-def test_load_data():
-  # consider multiple tests for each of the try and except block.
-  # be able to run without needing the file 
-  # mock pd.read_csv function
-  # consider testing passing data through the CLI.
-  ### python3 main.py --file ./my_file.csv
-  df = fs.load_data()
-  assert type(df.iloc[0]) == int or float
-
 def test_define_head_rank():
-  # make a pandas df in this block
-  df = fs.load_data()
+  df = pd.DataFrame({"lat":[35.765909,35.765341,35.765707],
+                     "lon":[-78.724288,-78.723918,-78.72343],
+                     "head":[387.5,375.2,360]})
   df = fs.__define_head_rank(df)
   assert df.loc[1.0].iloc[2] >= df.loc[2.0].iloc[2] >= df.loc[3.0].iloc[2]
 
-def test_create_geopy_points():
-  df = fs.load_data()
-  df = fs.define_head_rank(df)
-  low_point, mid_point, high_point = fs.create_geopy_points(df)
-  assert low_point.latitude == 35.765707
+# def test_create_geopy_points():
+#   df = fs.__load_data()
+#   df = fs.__define_head_rank(df)
+#   low_point, mid_point, high_point = fs.__create_geopy_points(df)
+#   assert low_point.latitude == 35.765707
 
-def test_create_head_variables():
-  df = fs.load_data()
-  df = fs.define_head_rank(df)
-  low_head, mid_head, high_head = fs.create_head_variables(df)
-  assert low_head == 360
+# def test_create_head_variables():
+#   df = fs.__load_data()
+#   df = fs.__define_head_rank(df)
+#   low_head, mid_head, high_head = fs.__create_head_variables(df)
+#   assert low_head == 360
 
-def test_length_low_to_high():
-  df = fs.load_data()
-  df = fs.define_head_rank(df)
-  low_point, mid_point, high_point = fs.create_geopy_points(df)
-  length = fs.length_low_to_high(low_point, high_point)
-  assert length == 264.9634085161976
+# def test_length_low_to_high():
+#   df = fs.__load_data()
+#   df = fs.__define_head_rank(df)
+#   low_point, mid_point, high_point = fs.__create_geopy_points(df)
+#   length = fs.__length_low_to_high(low_point, high_point)
+#   assert length == 264.9634085161976
 
-def test_get_bearing():
-  df = fs.load_data()
-  df = fs.define_head_rank(df)
-  low_point, mid_point, high_point = fs.create_geopy_points(df)
-  length = fs.length_low_to_high(low_point, high_point)
-  bearing = fs.get_bearing(low_point, high_point)
-  assert round(bearing) == 286
+# def test_get_bearing():
+#   df = fs.__load_data()
+#   df = fs.__define_head_rank(df)
+#   low_point, mid_point, high_point = fs.__create_geopy_points(df)
+#   length = fs.__length_low_to_high(low_point, high_point)
+#   bearing = fs.__get_bearing(low_point, high_point)
+#   assert round(bearing) == 286
 
-# test_get_bearing_returns_...expected_behavior
-# be able to read test name and know what went wrong under what conditions. 
+# # test_get_bearing_returns_...expected_behavior
+# # be able to read test name and know what went wrong under what conditions. 
 
-def test_equipotential_midpoint():
-  df = fs.load_data()
-  df = fs.define_head_rank(df)
-  low_point, mid_point, high_point = fs.create_geopy_points(df)
-  length = fs.length_low_to_high(low_point, high_point)
-  bearing = fs.get_bearing(low_point, high_point)
-  equipotential_point = fs.equipotential_midpoint(length, bearing)
-  assert round(equipotential_point.latitude, 6) == 35.765818
-  assert round(equipotential_point.longitude, 6) == -78.723905
+# def test_equipotential_midpoint():
+#   df = fs.__load_data()
+#   df = fs.__define_head_rank(df)
+#   low_point, mid_point, high_point = fs.__create_geopy_points(df)
+#   length = fs.__length_low_to_high(low_point, high_point)
+#   bearing = fs.__get_bearing(low_point, high_point)
+#   equipotential_point = fs.__equipotential_midpoint(length, bearing)
+#   assert round(equipotential_point.latitude, 6) == 35.765818
+#   assert round(equipotential_point.longitude, 6) == -78.723905
 
-def test_get_flow_azimuth():
-  df = fs.load_data()
-  df = fs.define_head_rank(df)
-  low_point, mid_point, high_point = fs.create_geopy_points(df)
-  length = fs.length_low_to_high(low_point, high_point)
-  bearing = fs.get_bearing(low_point, high_point)
-  equipotential_point = fs.equipotential_midpoint(length, bearing)
-  flow_azimuth = fs.get_flow_azimuth(mid_point, equipotential_point, low_point)
-  assert flow_azimuth == 91
+# def test_get_flow_azimuth():
+#   df = fs.__load_data()
+#   df = fs.__define_head_rank(df)
+#   low_point, mid_point, high_point = fs.__create_geopy_points(df)
+#   length = fs.__length_low_to_high(low_point, high_point)
+#   bearing = fs.__get_bearing(low_point, high_point)
+#   equipotential_point = fs.__equipotential_midpoint(length, bearing)
+#   flow_azimuth = fs.__get_flow_azimuth(mid_point, equipotential_point, low_point)
+#   assert flow_azimuth == 91
 
-  # end-to-end test. (smoke test)
-  # separate out into a different suite.
+#   # end-to-end test. (smoke test)
+#   # separate out into a different suite.
